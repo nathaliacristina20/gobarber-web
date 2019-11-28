090cb6f5d3d07bd319aeae4d742ff04533f469e6
Nathalia Cristina <nathaliacristina20@hotmail.com>
2019-11-27 21:55:41 -0300

    Estilização da autenticação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-    return <h1>Cadastro</h1>;
+    return (
+        <>
+            <img src={logo} alt="GoBarber" />
+            <form>
+                <input placeholder="Nome completo" />
+                <input type="email" placeholder="Seu e-mail" />
+                <input type="password" placeholder="Sua senha secreta" />
+                <button type="submit">Criar conta</button>
+                <Link to="/">Já tenho conta</Link>
+            </form>
+        </>
+    );
 }
