5c683bd677379183c0068b3a8f4f89b681715461
Nathalia Cristina <nathaliacristina20@hotmail.com>
2019-11-28 21:45:43 -0300

    Autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
     email: Yup.string()
@@ -12,8 +14,9 @@ const schema = Yup.object().shape({
     password: Yup.string().required('A senha é obrigatória'),
 });
 export default function SignIn() {
-    function handleSubmit(data) {
-        console.tron.log(data);
+    const dispatch = useDispatch();
+    function handleSubmit({ email, password }) {
+        dispatch(signInRequest(email, password));
     }
 
     return (
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,22 +5,22 @@ import { Route, Redirect } from 'react-router-dom';
 
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
+import store from '~/store';
 
 export default function RouteWrapper({
-    // eslint-disable-next-line react/prop-types
     component: Component,
-    // eslint-disable-next-line react/prop-types
     isPrivate,
     ...rest
 }) {
-    const signed = false;
+    const { signed } = store.getState().auth;
 
     if (!signed && isPrivate) {
+        console.tron.log('caiu aqui 1');
         return <Redirect to="/" />;
     }
 
     if (signed && !isPrivate) {
-        console.tron.log('caiu aqui');
+        console.tron.log('caiu aqui 2');
         return <Redirect to="/dashboard" />;
     }
 
