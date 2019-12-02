77d6e9c61dd39778ebb9d0c531d57063dc50c371
Nathalia Cristina <nathaliacristina20@hotmail.com>
2019-12-02 20:43:00 -0300

    Requisições autenticadas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -20,6 +20,8 @@ export function* signIn({ payload }) {
             return;
         }
 
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+
         yield put(signInSuccess(token, user));
 
         history.push('/dashboard');
@@ -45,7 +47,18 @@ export function* signUp({ payload }) {
         yield put(signFailure());
     }
 }
+
+export function setToken({ payload }) {
+    if (!payload) return;
+
+    const { token } = payload.auth;
+
+    if (token) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
 export default all([
+    takeLatest('persist/REYDRATE', setToken),
     takeLatest('@auth/SIGN_IN_REQUEST', signIn),
     takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
