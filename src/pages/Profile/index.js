d234a465494b72aafd2d8dbaa0dd501a26d93bfd
Nathalia Cristina <nathaliacristina20@hotmail.com>
2019-12-03 20:24:40 -0300

    Página de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+import { Container } from './styles';
 
 export default function Profile() {
-    return <h1>Profile</h1>;
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit(data) {}
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome completo" />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu endereço de email"
+                />
+                <hr />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Sua senha atual"
+                />
+                <Input
+                    type="password"
+                    name="newPassword"
+                    placeholder="Nova senha"
+                />
+                <Input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirmação de senha"
+                />
+                <button type="submit">Atualizar perfil</button>
+            </Form>
+            <button type="button">Sair</button>
+        </Container>
+    );
 }
